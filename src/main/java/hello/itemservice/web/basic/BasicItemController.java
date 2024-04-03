53e4d949dfa4a4acade0dba42cb7ff65c767ad29
정재욱 <wjdwodnr5452@naver.com>
2024-04-03 22:00:50 +0900

    RedirectAttributes

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -98,13 +99,22 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+   // @PostMapping("/add")
     public String addItemV5(Item item){
         // @ModelAttribute 생략 가능
         itemRepository.save(item);
         // model.addAttribute("item", item); @ModelAttribute("item") 생략 가능
 
         return "redirect:/basic/items/"+item.getId();
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes){
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
